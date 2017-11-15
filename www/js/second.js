1820b0b8967c26f1d6a52db25faab57a1e03025b
Cattleya S. Bergonia <30885667+KBergonia111295@users.noreply.github.com>
2017-11-15 21:50:03 +0800

    Add files via upload

--- www/js/second.js
+++ www/js/second.js
@@ -1,4 +1,4 @@
-document.getElementById('field_submit').addEventListener('click',function(){
+document.addEventListener('click',function(){
 	let input = document.getElementById('field_input').value;
 	if(input != ""){
 		let testCount = 0;
